69252905fb566be77d7cd449effc3ec98d324090
studanshu <bahetysudhanshu@gmail.com>
2016-04-13 16:07:02 +0530

    Scheme for Hotels and Flights Created

--- server/api/hotel/hotel.model.js
+++ server/api/hotel/hotel.model.js
@@ -1,11 +1,23 @@
 'use strict';
 
 import mongoose from 'mongoose';
+var Schema = mongoose.Schema;
 
-var HotelSchema = new mongoose.Schema({
-  name: String,
-  info: String,
-  active: Boolean
+//Schema to store the check in and check out time for each room in the hotel
+var bookingsObj = new Schema({
+	Check_In: { type: Date, default: Date.now},
+	Check_Out: { type: Date, default: Date.now}},
+	{ noId: true}
+);
+
+var HotelSchema = new Schema({
+  Room_No: Number,
+  Type: String,
+  Price: Number,
+  Bookings: [bookingsObj],//array of bookings for that particular room.
+  No_of_Person: Number
 });
+
+HotelSchema.index({ Room_No: 1}, { unique: true});
 
 export default mongoose.model('Hotel', HotelSchema);
