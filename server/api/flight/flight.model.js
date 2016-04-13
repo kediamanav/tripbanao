d30a0e7af4f6b8036449c64eabaf7ab17baf665f
studanshu <bahetysudhanshu@gmail.com>
2016-04-13 17:44:21 +0530

    variable name standardized

--- server/api/flight/flight.model.js
+++ server/api/flight/flight.model.js
@@ -6,18 +6,18 @@ var Schema = mongoose.Schema;
 //A tuple to represent the number of seats present during the particular day of the flight.
 var seatAvailableObj = new Schema({ 
 	date: Date,
-	no_of_seats: { type: Number, default: 4 }},
+	numberOfSeats: { type: Number, default: 4 }},
 	{ noId: true
 });
 
 var FlightSchema = new Schema({
-  Flight_No: String,
-  Origin: String,
-  Destination: String,
-  Depart_Time: { type: Date, default: Date.now },
-  Arrival_Time: { type: Date, default: Date.now },
-  Duration: Number,
-  RunningDays: [String], // An array of days when the flight runs weekly. [Mon, Tues, Wed]
+  flightNo: String,
+  origin: String,
+  destination: String,
+  departureTime: { type: Date, default: Date.now },
+  arrivalTime: { type: Date, default: Date.now },
+  duration: Number,
+  runningDays: [String], // An array of days when the flight runs weekly. [Mon, Tues, Wed]
   seatsAvailable: [seatAvailableObj] //Array to store seatAvailableObj Schema.
 });
 
--- server/api/hotel/hotel.model.js
+++ server/api/hotel/hotel.model.js
@@ -5,24 +5,24 @@ var Schema = mongoose.Schema;
 
 //Schema to store the check in and check out time for each room in the hotel
 var bookingsObj = new Schema({
-	Check_In: { type: Date, default: Date.now},
-	Check_Out: { type: Date, default: Date.now}},
+	checkIn: { type: Date, default: Date.now},
+	checkOut: { type: Date, default: Date.now}},
 	{ noId: true}
 );
 
 var HotelDetails = new Schema({
-  Room_No: Number,
-  Type: String,
-  Price: Number,
-  Bookings: [bookingsObj],//array of bookings for that particular room.
-  No_of_Person: Number},
+  roomNo: Number,
+  type: String,
+  price: Number,
+  bookings: [bookingsObj],//array of bookings for that particular room.
+  numberOfPerson: Number},
   { noId: true
 });
 
 //Assuming that there is single hotel per city. In that hotel, we store the details of the rooms.
 var HotelSchema = new Schema({
-  City: String,
-  HotelDescription: [HotelDetails]
+  city: String,
+  hotelDescription: [HotelDetails]
 });
 
 export default mongoose.model('Hotel', HotelSchema);
