b6dee98e39534ecbb6add996d0f0ccb02cbac82a
studanshu <bahetysudhanshu@gmail.com>
2016-04-13 15:03:35 +0530

    hotel folder created

--- server/routes.js
+++ server/routes.js
@@ -10,6 +10,7 @@ var path = require('path');
 module.exports = function(app) {
 
   // Insert routes below
+  app.use('/api/hotels', require('./api/hotel'));
   app.use('/api/flights', require('./api/flight'));
   
   // All undefined asset or api routes should return a 404
