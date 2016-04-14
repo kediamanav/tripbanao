9fc8b40a3cdaf6350f39a33051468a410fa3487d
studanshu <bahetysudhanshu@gmail.com>
2016-04-14 20:22:41 +0530

    Flight search query done

--- server/config/seed.js
+++ server/config/seed.js
@@ -5,39 +5,62 @@
 
 'use strict';
 
-/*
-var Thing = require('../api/thing/thing.model');
 
+var Flight = require('../api/flight/flight.model');
 
-Thing.find({}).removeAsync()
+var compName = "Emirates"
+
+Flight.find({}).removeAsync()
   .then(function() {
-    Thing.create({
-      name: 'Development Tools',
-      info: 'Integration with popular tools such as Bower, Grunt, Karma, ' +
-             'Mocha, JSHint, Node Inspector, Livereload, Protractor, Jade, ' +
-             'Stylus, Sass, CoffeeScript, and Less.'
-    }, {
-      name: 'Server and Client integration',
-      info: 'Built with a powerful and fun stack: MongoDB, Express, ' +
-             'AngularJS, and Node.'
-    }, {
-      name: 'Smart Build System',
-      info: 'Build system ignores `spec` files, allowing you to keep ' +
-             'tests alongside code. Automatic injection of scripts and ' +
-             'styles into your index.html'
-    }, {
-      name: 'Modular Structure',
-      info: 'Best practice client and server structures allow for more ' +
-             'code reusability and maximum scalability'
-    }, {
-      name: 'Optimized Build',
-      info: 'Build process packs up your templates as a single JavaScript ' +
-             'payload, minifies your scripts/css/images, and rewrites asset ' +
-             'names for caching.'
-    }, {
-      name: 'Deployment Ready',
-      info: 'Easily deploy your app to Heroku or Openshift with the heroku ' +
-             'and openshift subgenerators'
+    Flight.create({
+      "companyName": compName,
+      "flightNo": "1234",
+      "from": "kolkata",
+      "to": "delhi",
+      "runningDays": ["Mon","Tue","Wed"],
+      "seatsAvailable":[
+          {
+              "date": "2011-10-12",
+              "numberOfSeats": 123
+          }
+      ]
+    }, 
+    {
+      "companyName": compName,
+      "flightNo": "1235",
+      "from": "mumbai",
+      "to": "delhi",
+      "runningDays": ["Fri","Sat","Sun"]
+    }, 
+    {
+      "companyName": compName,
+      "flightNo": "1236",
+      "from": "kolkata",
+      "to": "bangkok",
+    }, 
+    {
+      "companyName": compName,
+      "flightNo": "1237",
+      "from": "delhi",
+      "to": "mumbai",
+      "runningDays": ["Thu"],
+      "seatsAvailable":[
+          {
+              "date": "2011-10-12",
+              "numberOfSeats": 523
+          }
+      ]
+    }, 
+    {
+      "companyName": compName,
+      "flightNo": "1238",
+      "from": "mumbai",
+      "to": "kolkata",
+    }, 
+    {
+      "companyName": compName,
+      "flightNo": "1239",
+      "from": "kolkata",
+      "to": "delhi",
     });
   });
-*/
