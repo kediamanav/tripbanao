e4aca59034e677a587ffb10ecffc54010d3c84c5
kediamanav <Manav>
2016-04-14 15:20:01 +0530

    Hotels done

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('tripbanaoApp')
-  .controller('MainCtrl',['$scope', 'Travel', function($scope, Travel) {
+  .controller('MainCtrl',['$scope', '$location', 'FlightDetail','HotelDetail', function($scope, $location, FlightDetail, HotelDetail) {
     $scope.awesomeThings = [];
 
     $scope.popup1 = {
@@ -9,6 +9,14 @@ angular.module('tripbanaoApp')
     };
 
     $scope.popup2 = {
+      opened: false
+    };
+
+    $scope.popup3 = {
+      opened: false
+    };
+
+    $scope.popup4 = {
       opened: false
     };
 
@@ -20,20 +28,47 @@ angular.module('tripbanaoApp')
       $scope.popup2.opened = true;
     };
 
+    $scope.open3 = function() {
+      $scope.popup3.opened = true;
+    };
+
+    $scope.open4 = function() {
+      $scope.popup4.opened = true;
+    };
+
     $scope.flight = {};
     $scope.flight.count=1;
+    $scope.type = 1;
+
+    $scope.hotel = {};
+    $scope.hotel.count=1;
+    $scope.hotel.nights=1;
 
     $scope.cityCount =0 ;
+    $scope.roomCount =0 ;
 
     $scope.increaseCity = function(){
       $scope.cityCount = $scope.cityCount + 1;
     };
 
+    $scope.increaseRoom = function(){
+      $scope.roomCount = $scope.roomCount + 1;
+    };
+
     $scope.searchFlights = function(){
+
       console.log($scope.flight);
-      Travel.update({type:1} , $scope.flight, function success(value){
-        console.log("Done successfully");
-        console.log(value);
-      });
-    }
+      FlightDetail.putFlight($scope.flight);
+
+      $location.path("/flights");
+    };
+
+    $scope.searchHotels = function(){
+
+      console.log($scope.hotel);
+      HotelDetail.putHotel($scope.hotel);
+
+      $location.path("/hotels");
+    };
+    
   }]);
